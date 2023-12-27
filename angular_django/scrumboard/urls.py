03c5293ea261e23f9997f3f889e6c8c384727f74
Abel <se.abel.tadesse@gmail.com>
2023-12-27 23:18:23 +0300

    Adding swagger and Angular

--- angular_django/scrumboard/urls.py
+++ angular_django/scrumboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from .api import ListApi, CardApi
 
 urlpatterns = [
     path('', ListApi.as_view()),
     path('lists', ListApi.as_view()),
-    path('cards', CardApi.as_view())
+    path('cards', CardApi.as_view()),
+    path('home', TemplateView.as_view(template_name="scrumboard/home.html")),
 ]
